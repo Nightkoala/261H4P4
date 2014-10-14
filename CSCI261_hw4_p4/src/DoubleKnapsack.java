8b4d267dcb5e0cf33bb4b8ff837cfcfff49f1070
Derek Brown <djb3718@rit.edu>
2014-10-14 14:27:11 -0400

    Added to Eclipse and created java file

---
+++ CSCI261_hw4_p4/src/DoubleKnapsack.java
@@ -0,0 +1,15 @@
+/**
+ * DoubleKnapsack.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ *
+ * Purpose	Variation on the knapsack problem,  Find the max cost of items that
+ * 			can fit in both of the backpacks.
+ */
+
+public class DoubleKnapsack {
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end DoubleKnapsack
